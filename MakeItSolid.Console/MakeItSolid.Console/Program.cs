4fa80a1f7182b1e8c210f4f090ab5f35c7191fee
87018 <87018@TIADM69>
2019-07-23 16:50:54 -0300

    Adicionado arquivos.

---
+++ MakeItSolid.Console/MakeItSolid.Console/Program.cs
@@ -0,0 +1,49 @@
+﻿using MakeItSolid.Console;
+using System.Diagnostics;
+
+namespace MakeItSolid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string optionControl = "0";
+            do
+            {
+                System.Console.WriteLine("Escolha uma opção: \n 0 - Sair \n 1- SRP");
+                optionControl = System.Console.ReadLine();
+                System.Console.Clear();
+
+                switch (optionControl)
+                {
+                    case "1":
+
+                        #region Using Code For Single Responsibility Principle
+                        Journal j = new Journal();
+                        j.AddEntry("I cried Today");
+                        j.AddEntry("I ate a bug");
+                        System.Console.WriteLine(j);
+
+                        var p = new Persistence();
+                        var filename = @"C:\Users\87018\Source\Repos\MakeItSolid\MakeItSolid.Console\MakeItSolid.Console\Single Responsibility Principle\journal.txt";
+                        p.SaveToFile(j, filename);
+                        Process.Start(filename);
+                        #endregion
+                        break;
+
+                    case "0":
+                        System.Console.WriteLine("Saindo...");
+                        break;
+
+                    default:
+                        System.Console.WriteLine("Opção Inválida");
+                        break;
+                }
+
+                System.Console.ReadKey();
+                System.Console.Clear();
+            }
+            while (optionControl != "0");
+        }
+    }
+}
---
+++ MakeItSolid.Console/MakeItSolid.Console/Single Responsibility Principle/SingleResponsabilityPrinciple.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace MakeItSolid.Console
+{
+    public class Journal
+    {
+        private readonly List<string> entries = new List<string>();
+        private static int count = 0;
+
+        public int AddEntry(string text)
+        {
+            entries.Add($"{count++}:{text}");
+            return count;
+        }
+
+        public void RemoveEntry(int index)
+        {
+            entries.RemoveAt(index);
+        }
+
+        public override string ToString()
+        {
+            return string.Join(Environment.NewLine, entries);
+        }
+
+        #region Métodos que violam a SRP
+        ///Esses métodos violam a SRP pois adicionam mais responsábilidades à classe Journal.
+        ///Agora a classe Journal tem a responsabilidade de manter as entries e gerenciar as funcionalidades de persistência.
+        ///
+        //public void Save(string fileName)
+        //{
+        //    File.WriteAllText(fileName, ToString());
+        //}
+
+        //public static Journal Load(string fileName)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        //public void Load(Uri uri)
+        //{
+
+        //}
+        #endregion
+    }
+
+    public class Persistence
+    {
+        public void SaveToFile(Journal j, string fileName, bool overwrite = false)
+        {
+            if (overwrite || !File.Exists(fileName))
+            {
+                File.WriteAllText(fileName, j.ToString());
+            }
+        }
+    }
+}
